b343f9d125ea5c74c7686e06866d11630efeafcf
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2024-09-15 17:49:03 +0800

    merged_proxies.py

--- merged_proxies.py
+++ merged_proxies.py
@@ -386,6 +386,7 @@ def process_xray(data, index):
 merged_proxies = []
 
 # 处理不同类型的节点
+# process_urls('./urls/clash_quick.txt', process_clash)
 process_urls('./urls/clash_urls.txt', process_clash)
 # process_urls('./urls/hysteria_urls.txt', process_hysteria)
 process_urls('./urls/sb_urls.txt', process_sb)
